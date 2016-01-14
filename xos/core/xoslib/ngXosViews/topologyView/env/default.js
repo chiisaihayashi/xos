9b123e6df83dd7f830215dbf4b1f6e9f3a383978
Matteo Scandolo <teo@onlab.us>
2016-01-14 12:12:12 -0800

    Topology Environment

---
+++ xos/core/xoslib/ngXosViews/topologyView/env/default.js
@@ -0,0 +1,13 @@
+// This is a default configuration for your development environment.
+// You can duplicate this configuration for any of your Backend Environments.
+// Different configurations are loaded setting a NODE_ENV variable that contain the config file name.
+// `NODE_ENV=local npm start`
+//
+// If xoscsrftoken or xossessionid are not specified the browser value are used
+// (works only for local environment as both application are served on the same domain)
+
+module.exports = {
+  host: 'http://apt045.apt.emulab.net:9999/',
+  xoscsrftoken: 'S7EnvVCqf8O95TMo1PxWGafRZAZeoPEW',
+  xossessionid: 'uexax6kg00a6gq24he0xw34skzks64au'
+};
