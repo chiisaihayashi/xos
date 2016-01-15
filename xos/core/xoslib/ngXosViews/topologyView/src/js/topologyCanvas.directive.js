eee429a44614de8275f7d76388e0a65f92af0573
Matteo Scandolo <teo@onlab.us>
2016-01-15 09:47:05 -0800

    Topology view demo

--- xos/core/xoslib/ngXosViews/topologyView/src/js/topologyCanvas.directive.js
+++ xos/core/xoslib/ngXosViews/topologyView/src/js/topologyCanvas.directive.js
@@ -22,18 +22,17 @@ angular.module('xos.topologyView')
         .append('svg')
         .style('width', '600px')
         .style('height', '600px')
-        .style('background-color', 'black');
 
       const color = d3.scale.category20();
 
       const force = d3.layout.force()
-          .charge(-120)
-          .linkDistance(30)
+          .charge(-520)
           .size([config.width, config.height]);
 
       force
         .nodes(this.topologyNodes)
         .links(this.topologyLinks)
+        .linkDistance(80)
         .start();
 
       var link = svg.selectAll('.link')
@@ -43,24 +42,117 @@ angular.module('xos.topologyView')
           .style('stroke-width', function(d) { return Math.sqrt(d.value); });
 
       var node = svg.selectAll('.node')
-          .data(this.topologyNodes)
-        .enter().append('circle')
-          .attr('class', 'node')
-          .attr('r', 10)
-          .style('fill', function(d) { return color(d.group); })
+        .data(this.topologyNodes, d => d.id)
+        .enter()
+        .append('g')
+        .attr({
+          class: d => d.type,
+          transform: (d) => {
+            return 'translate(' + d.x + ',' + d.y + ')';
+          }
+        });
+
+      const fab = node.filter('.fabric-switch');
+      const ovs = node.filter('.ovs-switch');
+      const bbu = node.filter('.bbu');
+      const rru = node.filter('.rru');
+
+      fab.each(function(item){
+        var node = d3.select(this);
+        node
+          .append('rect')
+          .attr('class', `node node-${item.type}`)
+          .attr('width', 20)
+          .attr('height', 20)
+          .attr('x', -10)
+          .attr('y', -10)
+          .style('fill', function(d) { return color(d.type); })
           .call(force.drag);
 
-      force.on('tick', function() {
-        link.attr('x1', function(d) { 
-              // debugger;
-              return d.source.x; 
-            })
-            .attr('y1', function(d) { return d.source.y; })
-            .attr('x2', function(d) { return d.target.x; })
-            .attr('y2', function(d) { return d.target.y; });
+        node
+          .append('text')
+          .text((d) => d.label)
+          .style('fill', function(d) { return color(d.type); })
+          .attr('x', -30)
+          .attr('y', 25)
+      })
 
-        node.attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
+      ovs.each(function(item){
+        var node = d3.select(this);
+        node
+          .append('rect')
+          .attr('class', `node node-${item.type}`)
+          .attr('width', 20)
+          .attr('height', 20)
+          .attr('x', -10)
+          .attr('y', -10)
+          .style('fill', function(d) { return color(d.type); })
+          .call(force.drag);
+
+        node
+          .append('text')
+          .text((d) => d.label)
+          .style('fill', function(d) { return color(d.type); })
+          .attr('x', -30)
+          .attr('y', 25)
+      })
+
+      bbu.each(function(item){
+        var node = d3.select(this);
+        node
+          .append('ellipse')
+          .attr('class', `node node-${item.type}`)
+          .attr('rx', 20)
+          .attr('ry', 10)
+          .style('fill', function(d) { return color(d.type); })
+          .call(force.drag);
+
+        node
+          .append('text')
+          .text((d) => d.label)
+          .style('fill', function(d) { return color(d.type); })
+          .attr('x', -30)
+          .attr('y', 25)
+      })
+
+      rru.each(function(item){
+        var node = d3.select(this);
+        // area
+        node
+          .append('circle')
+          .attr('class', 'node-area')
+          .attr('r', 40)
+          .style('fill', function(d) { return color(d.type); })
+          .call(force.drag);
+
+        node
+          .append('circle')
+          .attr('class', `node node-${item.type}`)
+          .attr('r', 10)
+          .style('fill', function(d) { return color(d.type); })
+          .call(force.drag);
+
+        node
+          .append('text')
+          .text((d) => d.label)
+          .style('fill', function(d) { return color(d.type); })
+          .attr('x', -30)
+          .attr('y', 25)
+      })
+
+      force.on('tick', function() {
+        link.attr('x1', function(d) {
+          return d.source.x;
+        })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
+
+        node.attr({
+          transform: (d) => {
+            return 'translate(' + d.x + ',' + d.y + ')';
+          }
+        });
       });
 
     }
